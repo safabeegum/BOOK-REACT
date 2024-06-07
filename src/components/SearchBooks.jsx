47b4274161cbc46a4254a185b9d0d080412e5259
safabeegum <pasafabeegum@gmail.com>
2024-06-07 12:58:20 +0530

    Completed

--- src/components/SearchBooks.jsx
+++ src/components/SearchBooks.jsx
@@ -6,7 +6,7 @@ const SearchBooks = () => {
         <div className="container">
             <div className="row">
                 <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-                    <div className="row">
+                    <div className="row g-3">
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
                             <label htmlFor="" className="form-label">BOOK TITLE</label>
                             <input type="text" className="form-control" />
