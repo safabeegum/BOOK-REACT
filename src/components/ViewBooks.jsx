cff9db4cc7a72d09146bc6b8646f3a339fe7ca2d
safabeegum <pasafabeegum@gmail.com>
2024-06-07 13:06:24 +0530

    Completed

--- src/components/ViewBooks.jsx
+++ src/components/ViewBooks.jsx
@@ -9,7 +9,82 @@ const ViewBooks = () => {
                     <div className="row g-3">
                         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
-                            
+                        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">TITLE</th>
+      <th scope="col">AUTHOR</th>
+      <th scope="col">PUBLISHING YEAR</th>
+      <th scope="col">PRICE</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>  
+      <th scope="row">1</th>
+      <td>Harry Potter</td>
+      <td></td>
+      <td>@mdo</td>
+      <td></td>
+    </tr>
+
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+      <td></td>
+    </tr>
+    
+    <tr>
+    <th scope="row">3</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+      <td></td>
+    </tr>
+
+    <tr>
+    <th scope="row">4</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+      <td></td>
+    </tr>
+      
+    <tr>
+    <th scope="row">5</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+      <td></td>
+    </tr>
+    
+    <tr>
+    <th scope="row">6</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+      <td></td>
+    </tr>
+
+    <tr>
+    <th scope="row">7</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+      <td></td>
+    </tr>
+
+    <tr>
+    <th scope="row">8</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+      <td></td>
+    </tr>
+  </tbody>
+</table>
                         </div>
                     </div>
                 </div>
